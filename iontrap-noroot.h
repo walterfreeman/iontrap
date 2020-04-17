afac1956c3dbae7e817222f5a14c40b66ab788d8
Walter Freeman <wafreema@syr.edu>
2020-04-17 11:48:15 -0400

    sdf

--- iontrap-noroot.h
+++ iontrap-noroot.h
@@ -166,7 +166,7 @@ class ionTrap {
 
 	if (checktimer(0) > 0.1)
 	{
-	  printf("Runtime = %f sec; copmleted %d steps, rate %f steps per second; particle at radius %e; radratio %e\n",checktimer(1),steps,steps/checktimer(1),mag(r), mag(r) / (t * mag(Fext)/drag));
+	  printf("Runtime = %f sec; copmleted %d steps, rate %.2e steps per second; particle at radius %e; radratio %e\n",checktimer(1),steps,steps/checktimer(1),mag(r), mag(r) / (t * mag(Fext)/drag));
 	  starttimer(0);
 	}
 
